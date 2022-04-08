b83fa85b4ee7c30cae4f12381cf3e01b42590e1f
Choff3 <teddyhoff@gmail.com>
2022-04-08 15:25:06 -0400

    Updated scripts for dedicated Dash user

--- scripts/Old/update-fortune.sh
+++ scripts/Old/update-fortune.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 file="/home/pi/messages/dailyfortune.txt"
+tempfile="/home/pi/messages/forttemp.txt"
 rm $file
-/usr/games/fortune >> $file
-nlines=$(cat messages/dailyfortune.txt | wc -l)
-if [[ $nlines -lt 5 ]]
-then
-  sed -i " " $file
-  declare -i dif=$((5-$nlines))
-  for ((i=1;i<=dif;i++));
-  do
-    echo "*" >> $file
-  done
-fi
+rm $tempfile
+/usr/games/fortune >> $tempfile
+
+fold -w 39 -s $tempfile >> $file
+
+nlines=$(cat $file | wc -l)
+
+while [ $nlines -gt 5 ]
+do
+  rm $file
+  /usr/games/fortune >> $file
+  nlines=$(cat $file | wc -l)
+done
+
+dif=$(( 5 - nlines ))
+for ((i=1;i<=$dif;i++));
+do
+  echo " " >> $file
+done
