64a739a98782fffdd55993f632fae5a80b521756
Teddy Hoff <42592276+Choff3@users.noreply.github.com>
2022-06-12 18:52:03 -0400

    Update dashinit.sh

--- scripts/dashinit.sh
+++ scripts/dashinit.sh
@@ -10,7 +10,7 @@ tmux select-pane -t 2
 sleep 20
 tmux split-window -h
 tmux resize-pane -L 42
-tmux send-keys -t 0 C-z 'tty-clock -sScbt -C 6 -f "%A, %B %e, %Y"' Enter
+tmux send-keys -t 0 C-z 'tty-clock -sScbt -C 6 -f "%A %n %B %e, %Y"' Enter
 tmux send-keys -t 1 C-z '/home/dash/scripts/neofresh.sh' Enter
 tmux send-keys -t 2 C-z '/home/dash/scripts/cowfort.sh' Enter
 tmux send-keys -t 3 C-z 'sleep 20 && /home/dash/scripts/weather.sh' Enter
